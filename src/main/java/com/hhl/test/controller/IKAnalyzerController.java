1fc6ab8c3e88c56389c1e59835ec74958df41968
hhl <bbyshp@126.com>
2014-06-08 21:56:58 +0800

    Initial commit

---
+++ src/main/java/com/hhl/test/controller/IKAnalyzerController.java
@@ -0,0 +1,43 @@
+package com.hhl.test.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.wltea.analyzer.core.IKSegmenter;
+import org.wltea.analyzer.core.Lexeme;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 分词控制器
+ * Created by hhl on 2014/6/7.
+ */
+@Controller
+@RequestMapping("/ik")
+public class IKAnalyzerController {
+
+    @ResponseBody
+    @RequestMapping(value="split")
+    public String splitWord(String text,String isMaxLength) throws IOException {
+
+        StringReader re = new StringReader(text);
+        IKSegmenter ik = new IKSegmenter(re,isMaxLength.equals("1")?true:false);
+
+        String splited="";
+
+        Lexeme lex = null;
+
+        List<String> list=new ArrayList<String>();
+
+        while((lex=ik.next())!=null){
+            list.add(lex.getLexemeText());
+        }
+
+        return JSONObject.toJSONString(list);
+    }
+
+}
